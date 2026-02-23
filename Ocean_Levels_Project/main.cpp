ef7eef9c562b55fc7f15a00db188bf282c73ca6e
iStormathy <stormageddon473@yahoo.com>
2026-02-22 20:49:19 -0500

    Reorganized the lines 10 through 14 to match the pseudocode in the report. Changed a mistake in the text in lines 12 and 14

--- Ocean_Levels_Project/main.cpp
+++ Ocean_Levels_Project/main.cpp
@@ -7,10 +7,10 @@ using namespace std;
 int main() {
 	double lvlC = 1.5; // The distance (in millimeters) the ocean rises in a year
 	double year5 = lvlC * 5;
-	double year7 = lvlC * 7;
-	double year10 = lvlC * 10;
 	cout << "In 5 years, the ocean will be " << year5 << " millimeters higher than its current level.\n";
-	cout << "In 5 years, the ocean will be " << year7 << " millimeters higher than its current level.\n";
-	cout << "In 5 years, the ocean will be " << year10 << " millimeters higher than its current level.\n";
+	double year7 = lvlC * 7;
+	cout << "In 7 years, the ocean will be " << year7 << " millimeters higher than its current level.\n";
+	double year10 = lvlC * 10;
+	cout << "In 10 years, the ocean will be " << year10 << " millimeters higher than its current level.\n";
 	return 0;
 }
